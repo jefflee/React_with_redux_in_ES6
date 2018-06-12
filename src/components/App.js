d5216eb568ea84ec2daf67b7b5475660199145b5
Jeff <jeff@JeffdeMBP.Home>
2018-06-12 23:52:36 +0800

    Initial App structure

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+// This component handles the App template used on every page.
+import React, {PropTypes} from 'react';
+import Header from './common/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header
+          //loading={this.props.loading}
+        />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+  //loading: PropTypes.bool.isRequired
+};
+
+export default App;
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    const {courses} = this.props;
+
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
