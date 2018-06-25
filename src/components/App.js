da7440bc77d5104f4c23030c42f16c79cabc9423
Jeff <jeff@JeffdeMBP.Home>
2018-06-25 22:29:39 +0800

    1.display courses 2.add courses 3.update courses

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
         <Header
-          //loading={this.props.loading}
+          loading={this.props.loading}
         />
         {this.props.children}
       </div>
@@ -16,8 +17,14 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  children: PropTypes.object.isRequired
-  //loading: PropTypes.bool.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
